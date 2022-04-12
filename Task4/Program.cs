861d0b7bd41032d19bbb2b3498e820f2de5ba70a
Ekyd <Ekyd777@gmail.com>
2022-04-12 20:53:44 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Задача №4:На ввод число, программа должна вывести все четные числа от 1 до введенного: ");
+Console.WriteLine("Введите число: ");
+int number = Console.ReadLine();
+int index = 0;
+while (index >= number)
+    {
+        
+        index++;
+    }
+Console.WriteLine();
