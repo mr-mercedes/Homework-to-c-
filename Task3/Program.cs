5770699f510e249824b60ddf06d7f554641667a0
Ekyd <Ekyd777@gmail.com>
2022-04-12 20:29:19 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задача №3:На ввод число, программа должна определить четное оно или нет: ");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (Convert.ToBoolean(number %= 2)) 
+    {
+        Console.WriteLine("Число не четное!");
+    }
+else
+    {
+        Console.WriteLine("Число четное!");
+    }
