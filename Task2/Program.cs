67037f2ac21ebc39c6f590c286e67e4dc4faddc2
Ekyd <Ekyd777@gmail.com>
2022-04-12 19:08:35 +0300

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Задача №1:На ввод 3 числа, программа должна определить большее из них число: ");
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (max > number2)
+    {
+        if (max > number3)
+            {
+                Console.WriteLine("Наибольшее число из введенных: " + max);
+            }
+        else
+            {
+                Console.WriteLine("Наибольшее число из введенных: " + number3);
+
+            }
+
+            
+
+    }
+else
+    {
+        number2 = max;
+        if (max > number3)
+            {
+                Console.WriteLine("Наибольшее число из введенных: " + max);
+            }
+        else
+            {
+                Console.WriteLine("Наибольшее число из введенных: " + number3);
+            }
+    }
+
