da88c6b8b420f092da845ef6106f8d07c5a7ffe8
Ekyd <Ekyd777@gmail.com>
2022-04-17 14:53:29 +0300

    task5

---
+++ Task5/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Задача №5: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.");
+Console.WriteLine("Введите число: ");
+string? number = Console.ReadLine();
+Console.WriteLine(number?[2]);
+
